74b0dc963e4aec5d014a65f2bd9b1d6e7bebd899
Icaro Monteiro <icarodavi@gmail.com>
2022-05-25 19:23:55 -0300

    Swagger

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,8 @@
+import swaggerUI from "swagger-ui-express";
+
 import { app } from ".";
+import swaggerFile from "../swaggerFile.json";
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.listen(3333, () => console.log("Server is running!"));
